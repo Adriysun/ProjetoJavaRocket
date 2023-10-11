f02c7e43be3227e7b02c3faa2de09a759b9a02f0
Adrian <adrian.rodrigues0806@gmail.com>
2023-10-10 21:54:41 -0300

    .

---
+++ todolist/src/test/java/br/com/adrianrodrigues/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.adrianrodrigues.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
